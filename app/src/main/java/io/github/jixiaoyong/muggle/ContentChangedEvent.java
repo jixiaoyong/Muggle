78fff95ac31341faf0c64ba2d47d84208816a347
jixiaoyong <jixiaoyong1995@gmail.com>
2019-04-25 19:33:22 +0800

    clean all files in gh-pages

--- app/src/main/java/io/github/jixiaoyong/muggle/ContentChangedEvent.java
+++
@@ -1,9 +0,0 @@
-package io.github.jixiaoyong.muggle;
-
-public class ContentChangedEvent {
-    public String content;
-
-    public ContentChangedEvent(String content) {
-        this.content = content;
-    }
-}
